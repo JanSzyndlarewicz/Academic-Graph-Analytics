4c492392d049965efb3d7a85437f747031869eb2
janek_szyndlarewicz <jan.szyndlarewicz@gmail.com>
2024-12-01 19:25:50 +0100

    Dataset handler update

--- data_retrival/semantic_scholar/detasets_handler.py
+++ data_retrival/semantic_scholar/detasets_handler.py
@@ -6,3 +6,12 @@ class DatasetsScholarAPI(APIClient):
     def __init__(self):
         super().__init__()
         self.base_url += SEMANTIC_SCHOLAR_DATASETS_BASE_URL
+
+    def get_available_releases(self) -> dict:
+        return self.fetch_data("releases", "releases.json")
+
+    def get_datasets_listing(self, release_date: str) -> dict:
+        return self.fetch_data(f"release/{release_date}", "datasets.json")
+
+    def get_links_for_dataset(self, release_date: str, dataset_name: str) -> dict:
+        return self.fetch_data(f"release/{release_date}/dataset/{dataset_name}", "links.json")

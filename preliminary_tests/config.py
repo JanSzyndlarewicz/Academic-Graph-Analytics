b724285cc93dad64069bfffab943781223e07745
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-12-07 20:37:54 +0100

    pulling countries

--- preliminary_tests/config.py
+++ preliminary_tests/config.py
@@ -7,3 +7,175 @@ load_dotenv()
 SCOPUS_KEY = os.getenv("SCOPUS_KEY")
 PAPER_SEARCH_URL = "https://api.elsevier.com/content/search/scopus"
 SAFETY_LIMIT = 25
+
+country2uni_institutions_names = {
+    "Germany": [
+        "Helmholtz Gemeinschaft",
+        "Max Planck Gesellschaft",
+        "European Molecular Biology Laboratory Heidelberg",
+        "European Molecular Biology Organization",
+        "Leibniz Gemeinschaft",
+        "Technische Universitat Munchen",
+        "Max Planck Institut fur Informatik",
+        "Deutsches Krebsforschungszentrum",
+        "Max Planck Institut fur Intelligente Systeme"
+    ],
+    "Poland": [
+        "Polish Academy of Sciences",
+        "Jagiellonian University",
+        "University of Warsaw",
+        "Mossakowski Medical Research Centre Polish Academy of Sciences",
+        "Nicolaus Copernicus University",
+        "Adam Mickiewicz University",
+        "University of Lodz",
+        "AGH University of Science and Technology",
+        "Medical University of Warsaw",
+        "Medical University of Lodz"
+    ],
+    "Russian Federation": [
+        "Russian Academy of Sciences",
+        "Lomonosov Moscow State University",
+        "Saint Petersburg State University",
+        "Institute of Chemical Biology and Fundamental Medicine Russian Academy of Sciences",
+        "State University - Higher School of Economics",
+        "Ministry of Science and Higher Education",
+        "I.M. Sechenov First Moscow State Medical University",
+        "Peter the Great St Petersburg Polytechnic University",
+        "Shemyakin-Ovchinnikov Institute of Bioorganic Chemistry of the Russian Academy of Sciences",
+        "Ural Federal University"
+    ],
+    "United Kingdom": [
+        "University of Oxford",
+        "University College London",
+        "European Bioinformatics Institute EMBL",
+        "University of Cambridge",
+        "Imperial College London",
+        "Kings College London",
+        "Medical Research Council",
+        "The University of Edinburgh",
+        "The University of Manchester"
+    ],
+    "United States": [
+        "Harvard University",
+        "Harvard Medical School",
+        "U.S. Department of Health & Human Services",
+        "National Institutes of Health",
+        "Stanford University",
+        "American Cancer Society",
+        "The New York Genome Center",
+        "Johns Hopkins University",
+        "Parker Institute for Cancer Immunotherapy",
+        "Massachusetts Institute of Technology"
+    ],
+    "China": [
+        "Chinese Academy of Sciences",
+        "Ministry of Education of the People's Republic of China",
+        "University of Chinese Academy of Sciences",
+        "Tsinghua University",
+        "Zhejiang University",
+        "Shanghai Jiao Tong University",
+        "Peking University",
+        "State Grid Corporation of China",
+        "Huazhong University of Science and Technology",
+        "Sichuan University"
+    ],
+    "Switzerland": [
+        "Ludwig Institute for Cancer Research Lausanne",
+        "Swiss Federal Institute of Technology",
+        "IBM Switzerland",
+        "Universitat Zurich",
+        "Ecole Polytechnique Federale de Lausanne",
+        "Novartis Institutes for Biomedical Research, Switzerland",
+        "Swiss Institute of Bioinformatics",
+        "Universitat Bern",
+        "Hoffmann-La Roche Ltd, Switzerland",
+        "Universite de Geneve"
+    ]
+}
+
+country2unis_names = {
+    "Russian Federation": [
+        "Lomonosov Moscow State University",
+        "Saint Petersburg State University",
+        "Krasnoyarsk State Agrarian University",
+        "State University - Higher School of Economics",
+        "Peter the Great St Petersburg Polytechnic University",
+        "Saint Petersburg State University of Architecture and Civil Engineering",
+        "Ural Federal University",
+        "Peoples' Friendship University of Russia",
+        "Saint Petersburg Mining University",
+        "Moscow State University of Civil Engineering"
+    ],
+    "United Kingdom": [
+        "University of Oxford",
+        "University College London",
+        "University of Cambridge",
+        "Imperial College London",
+        "The University of Edinburgh",
+        "The University of Manchester",
+        "Kings College London",
+        "University of Liverpool",
+        "University of Bristol",
+        "University of Nottingham"
+    ],
+    "United States": [
+        "Harvard University",
+        "Harvard Medical School",
+        "Stanford University",
+        "Johns Hopkins University",
+        "University of Pennsylvania",
+        "University of Michigan, Ann Arbor",
+        "University of Chicago",
+        "University of Washington",
+        "Columbia University",
+        "Massachusetts Institute of Technology"
+    ],
+    "Switzerland": [
+        "Swiss Federal Institute of Technology",
+        "Universitat Zurich",
+        "Ecole Polytechnique Federale de Lausanne",
+        "Universitat Bern",
+        "Universite de Geneve",
+        "Universitat Basel",
+        "Universite de Lausanne",
+        "Universita della Svizzera Italiana",
+        "Universite de Fribourg",
+        "Universitat St Gallen"
+    ],
+    "Poland": [
+        "Jagiellonian University",
+        "University of Warsaw",
+        "Nicolaus Copernicus University",
+        "University of Lodz",
+        "Adam Mickiewicz University",
+        "AGH University of Science and Technology",
+        "Warsaw University of Life Sciences",
+        "Wroclaw Medical University",
+        "University of Warmia and Mazury",
+        "Poznan University of Life Sciences"
+    ],
+    "Germany": [
+        "Hochschule fur Wirtschaft und Recht Berlin",
+        "Technische Universitat Munchen",
+        "Ludwig-Maximilians Universitat Munchen",
+        "Universitat Hamburg",
+        "Universitat Heidelberg",
+        "Friedrich-Alexander-Universitat Erlangen-Nurnberg",
+        "Rheinisch-Westfalische Technische Hochschule Aachen",
+        "Technische Universitat Dresden",
+        "Rheinische Friedrich-Wilhelms-Universitat Bonn",
+        "Karlsruher Institut fur Technologie"
+    ],
+    "China": [
+        "University of Chinese Academy of Sciences",
+        "Tsinghua University",
+        "Zhejiang University",
+        "Shanghai Jiao Tong University",
+        "Peking University",
+        "Huazhong University of Science and Technology",
+        "Sichuan University",
+        "Central South University",
+        "Sun Yat-Sen University",
+        "Fudan University"
+    ]
+}
--- preliminary_tests/fields_tests.py
+++ preliminary_tests/fields_tests.py
@@ -1,10 +1,14 @@
+from datetime import datetime
 import json
 import os
 import re
 from collections import Counter, defaultdict
-
+import textdistance
 import jsonlines
 import matplotlib.pyplot as plt
+import requests
+
+from config import country2uni_institutions_names, country2unis_names
 
 
 def get_fields_occurances(path):
@@ -109,21 +113,149 @@ def get_sample_field_values(path, field="affiliation", sample_size=10):
     return values
 
 
+# def get_best_affils_for_uni_names(save_path, country2uni_names):
+#     http_link_no_affil = "https://api.elsevier.com/content/search/affiliation?apiKey=c3818bd39b42c0db849380bbf544771e&query=affil({})"
+#     for country, uni_names in country2uni_names.items():
+#         for uni_name in uni_names:
+#             print(http_link_no_affil.format(uni_name))
+#             response = requests.get(http_link_no_affil.format(uni_name))
+#             with open(save_path, "w") as file:
+#                 json.dump(, file)
+                
+
+def pull_all_affils_matches_for_each_top_uni(top_universities):
+    # Load your Scopus API key (ensure you've set it in your environment variables)
+    SCOPUS_KEY = os.getenv("SCOPUS_KEY")
+    if not SCOPUS_KEY:
+        raise ValueError("SCOPUS_KEY not found. Make sure it's defined in your environment variables.")
+
+    base_url = "https://api.elsevier.com/content/search/affiliation"
+    return_dict = defaultdict(list)
+
+    for country, universities in top_universities.items():
+        for current_university in universities:
+            params = {"query": f"affil({current_university})", "apiKey": SCOPUS_KEY}
+            headers = {"X-ELS-APIKey": SCOPUS_KEY, "Accept": "application/json"}
+            try:
+                response = requests.get(base_url, params=params, headers=headers)
+                response.raise_for_status()  # Raise an HTTPError for bad responses
+                data = response.json()
+
+                # Parse the results
+                result_affils = data.get("search-results", {}).get("entry", [])
+                result_affils = [
+                    {
+                        "affiliation-name": affil.get("affiliation-name", "N/A"),
+                        "dc:identifier": affil.get("dc:identifier", "N/A"),
+                        "country": affil.get("country", "N/A"),
+                        "name_variant": affil.get("name-variant", [])[0].get("$", "N/A") if affil.get("name-variant") else "N/A",
+                        "document-count": affil.get("document-count", "N/A"),
+                        "city": affil.get("city", "N/A"),
+                        "eid": affil.get("eid", "N/A"),
+                    }
+                    for affil in result_affils
+                ]
+
+                return_dict[country].append({current_university: result_affils})
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching data for {current_university}: {e}")
+            except KeyError as e:
+                print(f"Unexpected data structure for {current_university}: {e}")
+
+    timestamp = datetime.now().strftime("%d-%H-%M")
+    # Write the results to a file
+    with open(f"best_affils_for_top_unis_{timestamp}.json", "w", encoding="utf-8") as file:
+        json.dump(return_dict, file, indent=4, ensure_ascii=False)
+
+    print("Data successfully saved to 'best_affils_for_top_unis.json'")
+
+def check_for_duplicates(path):
+    set_of_affils = defaultdict(list)
+    with open("./data/best_affils_for_top_unis_05-22-15.json", encoding="utf-8") as file:
+        print("Loading data...")
+        countries2uni2best_affils = json.load(file)
+        print("Data loaded.")
+        for country, uni2best_affils in countries2uni2best_affils.items():
+            for best_affils in uni2best_affils:
+                for affil_name, best_affils_pars in best_affils.items():
+                    for rank, best_affil_pars in enumerate(best_affils_pars):
+                        print(type(best_affil_pars))
+                        if best_affil_pars['affiliation-name'] in set_of_affils:
+                            print(f"Duplicate found: {best_affil_pars}")
+                        set_of_affils[best_affil_pars['affiliation-name']].append([rank, country, best_affil_pars])
+    duplicates_pairs = {}
+    for key, val in set_of_affils.items():
+        if len(val) > 1:
+            duplicates_pairs[key] = val
+    with open("./data/duplicates.json", "w", encoding="utf-8") as file:
+        json.dump(duplicates_pairs, file, indent=4, ensure_ascii=False)
+        
+def  check_which_unis_dont_have_exact_matches(path):
+    set_of_affils = {}
+    with open(path, encoding="utf-8") as file:
+        print("Loading data...")
+        countries2uni2best_affils = json.load(file)
+        print("Data loaded.")
+        for country, uni2best_affils in countries2uni2best_affils.items():
+            for best_affils in uni2best_affils:
+                for affil_name, best_affils_pars in best_affils.items():
+                    is_exact_match = False
+                    for rank, best_affil_pars in enumerate(best_affils_pars):
+                        if affil_name == best_affil_pars['affiliation-name']:
+                            # print(f"Exact match found:{affil_name} {best_affil_pars['affiliation-name']}")
+                            is_exact_match = True
+                            break
+                    if not is_exact_match:
+                        # print(f"No exact match found for {best_affil_pars}")
+                        # best_affils_pars = sorted(best_affils_pars,
+                        #                         #   key=lambda x: textdistance.levenshtein.normalized_similarity(affil_name, x['affiliation-name']),
+                        #                         key = lambda x: int(x["document-count"]),
+                        #                           reverse=True)
+                        print("Before sorting:")
+                        print(len(best_affils_pars))
+                        for item in best_affils_pars:
+                            print(f"{item['affiliation-name']} - {item.get('document-count')}")
+                        
+                        
+                        # best_affils_pars = sorted(
+                        #     best_affils_pars,
+                        #     key=lambda x: int(x.get("document-count", 0)),
+                        #     reverse=True
+                        # )
+                        best_affils_pars_sorted = sorted(best_affils_pars,
+                                                #   key=lambda x: textdistance.levenshtein.normalized_similarity(affil_name, x['affiliation-name']),
+                                                key = lambda x: int(x["document-count"]),
+                                                  reverse=True)
+
+                        print("After sorting:")
+                        print(len(best_affils_pars))
+                        for item in best_affils_pars:
+                            print(f"{item['affiliation-name']} - {item.get('document-count')}")
+                            
+                        set_of_affils[affil_name] = best_affils_pars_sorted
+    
+    with open("./data/without_clear_match.json", "w+", encoding="utf-8") as file:
+        json.dump(set_of_affils, file, indent=4, ensure_ascii=False)
+
 def main():
     # print(get_fields_occurances(moscow_path))
     # print(get_sample_field_values(moscow_path))
     # visualise_field_length_distribution(pwr_agriculture_path)
     # print(get_mean_difference_cover_cover_display_dates_years(moscow_path))
-    visualise_field_value_distribution(moscow_path, regex=r"^.{4}", field="prism:coverDate", show_every=1)
-    sub_field = (
-        # "affilname"
-        "affiliation-country"
-        # "affiliation-city"
-    )
+    # visualise_field_value_distribution(moscow_path, regex=r"^.{4}", field="prism:coverDate", show_every=1)
+    # sub_field = (
+    #     # "affilname"
+    #     "affiliation-country"
+    #     # "affiliation-city"
+    # )
     # visualise_affiliation_field_values_distribution(moscow_path, sub_field=sub_field, show_every=1)
 
     # regex allows for aggregation of months
     # visualise_field_value_distribution(pwr_agriculture_path, regex=r"(?<=-)[^-\d]*\d{2}", show_every=1)
+    # pull_all_affils_matches_for_each_top_uni(country2unis_names)
+    # check_for_duplicates("./data/best_affils_for_top_unis_06-02-13.json")
+    check_which_unis_dont_have_exact_matches("./data/best_affils_for_top_unis_06-02-13.json")
 
 
 if __name__ == "__main__":
@@ -132,3 +264,4 @@ if __name__ == "__main__":
     moscow_path = r"C:\Users\aleks\Pulpit\CarlosIII4sem\web analytics\final_project\Academic-Graph-Analytics\preliminary_tests\data\60021331-econ-batch.jsonl"
     pwr_agriculture_path = r"C:\Users\aleks\Pulpit\CarlosIII4sem\web analytics\final_project\Academic-Graph-Analytics\preliminary_tests\data\test_batch.jsonl"
     main()
+    
